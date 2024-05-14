b31e8fb8ba698fb73f652a18d4fe06420bcad909
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-05-14 23:44:50 +0100

    zakatgpt

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -14,12 +14,10 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-md sm:text-center">
           <h2 className="text-3xl font-medium tracking-tight text-white sm:text-4xl">
-            Get your first tips today
+            Business Zakat GPT
           </h2>
           <p className="mt-4 text-lg text-gray-300">
-            It takes 30 seconds to sign up. Download the app and create an
-            account today and we’ll send you a tip guaranteed to double your
-            first investment.
+            Every business has a responsibility to pay Zakat, however, not every business is the same when it comes to calculating Zakat. Our GPT model is designed to help businesses calculate their Zakat in a way that is both accurate and efficient.
           </p>
           <div className="mt-8 flex justify-center">
             <AppStoreLink color="white" />
